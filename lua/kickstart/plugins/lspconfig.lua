79981fb1bb0f94bca8d8fdcbbc300b3ba78defee
keeperofkey <mail@liamdodd.com>
2024-11-15 16:03:05 -0500

    ruff

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -147,7 +147,6 @@ return {
         -- clangd = {},
         -- gopls = {},
         -- rust_analyzer = {},
-        -- ruff_lsp = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
