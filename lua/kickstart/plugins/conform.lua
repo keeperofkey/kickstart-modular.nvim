39d3ba1f63f859c03a7563f4cc6dae77079eef8b
Liam <mail@liamdodd.com>
2026-03-07 09:42:23 -0500

    fix(conform): replace deprecated lsp_fallback with lsp_format

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -5,7 +5,7 @@ return {
       notify_on_error = false,
       format_on_save = {
         timeout_ms = 500,
-        lsp_fallback = true,
+        lsp_format = 'fallback',
       },
       formatters_by_ft = {
         lua = { 'stylua' },
