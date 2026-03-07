603731dc3356edf352fb407f8ae43bc2903c3b9f
Liam <mail@liamdodd.com>
2026-03-07 08:37:07 -0500

    fix(telescope): expand $HOME in find config files keymap

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -155,7 +155,7 @@ return {
         builtin.find_files { cwd = vim.fn.stdpath 'config' }
       end, { desc = '[F]ind [N]eovim files' })
       vim.keymap.set('n', '<leader>fx', function()
-        builtin.find_files { cwd = '$HOME/.config' }
+        builtin.find_files { cwd = vim.fn.expand '$HOME/.config' }
       end, { desc = '[F]ind [x] config files' })
     end,
   },
